b8c9604a17a25c7cd52cd44ec214ffbdff51282a
Pedro Rodrigues <medecau@gmail.com>
2018-12-04 07:10:45 +0100

    refactors client code
    move client code into main file with server code
    wraps server startup in if-main block

--- datasetdb.py
+++ datasetdb.py
@@ -1,6 +1,45 @@
+from collections import namedtuple
+from functools import partial
+
 from playhouse.dataset import DataSet
 import zerorpc
 
+
+class Table:
+    def __init__(self, c, name):
+        self.c = c
+        self.name = name
+
+    def insert(self, **kwargs):
+        return self.c.insert(self.name, kwargs)
+
+    def update(self, **kwargs):
+        return self.c.update(self.name, kwargs)
+
+    def delete(self, **kwargs):
+        return self.c.delete(self.name, kwargs)
+
+    def find(self, **kwargs):
+        return self.c.find(self.name, kwargs)
+
+    def find_one(self, **kwargs):
+        return self.c.find_one(self.name, kwargs)
+
+    def all(self):
+        return self.c.all(self.name)
+
+    def __iter__(self):
+        return self.all()
+
+
+class Client:
+    def __init__(self, address=None):
+        if address is None:
+            address = 'tcp://127.0.0.1:4242'
+        self.c = zerorpc.Client(address)
+
+    def __getitem__(self, name):
+        return Table(self.c, name)
 
 class Service:
     def __init__(self):
@@ -26,15 +65,13 @@ class Service:
     def all(self, table):
         return self.db[table].all()
 
-
-srv = zerorpc.Server(Service())
-srv.bind('tcp://0.0.0.0:4242')
-srv.run()
+if __name__ == '__main__':
+    srv = zerorpc.Server(Service())
+    srv.bind('tcp://0.0.0.0:4242')
+    srv.run()
 
 
 """
 TODO:
-- bring the client code here
-- setup the server inside if __main__ block
 - use argparse to get binding address
 """
--- test.py
+++ test.py
@@ -1,44 +1,4 @@
-from collections import namedtuple
-from functools import partial
-import zerorpc
-
-
-table = namedtuple('Table', 'insert update delete find find_one all'.split(' '))
-class Table:
-    def __init__(self, c, name):
-        self.c = c
-        self.name = name
-
-    def insert(self, **kwargs):
-        return self.c.insert(self.name, kwargs)
-
-    def update(self, **kwargs):
-        return self.c.update(self.name, kwargs)
-
-    def delete(self, **kwargs):
-        return self.c.delete(self.name, kwargs)
-
-    def find(self, **kwargs):
-        return self.c.find(self.name, kwargs)
-
-    def find_one(self, **kwargs):
-        return self.c.find_one(self.name, kwargs)
-
-    def all(self):
-        return self.c.all(self.name)
-
-    def __iter__(self):
-        return self.all()
-
-
-class Client:
-    def __init__(self, address=None):
-        if address is None:
-            address = 'tcp://127.0.0.1:4242'
-        self.c = zerorpc.Client(address)
-
-    def __getitem__(self, name):
-        return Table(self.c, name)
+from datasetdb import Client
 
 
 c = Client()
